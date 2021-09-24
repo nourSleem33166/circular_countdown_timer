ff85dfbf9dbd60326e4619ac8542e2314eaaa3aa
Nour3316611@gmail.com <Nour3316611@gmail.com>
2021-09-24 19:27:32 +0300

    test

--- lib/circular_countdown_timer.dart
+++ lib/circular_countdown_timer.dart
@@ -120,7 +120,7 @@ class CircularCountDownTimerState extends State<CircularCountDownTimer>
       } else if (widget.textFormat == CountdownTextFormat.S) {
         return "0";
       } else {
-        return "00:00:00";
+        return "00:00:00 ";
       }
     } else {
       Duration duration = _controller!.duration! * _controller!.value;
